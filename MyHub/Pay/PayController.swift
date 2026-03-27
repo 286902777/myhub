e738b7e36b57fdd190a465eb05a41e1532e67ae8
myhub-ios <>
2026-03-27 02:44:34 -0700

    add vip btn

--- MyHub/Pay/PayController.swift
+++ MyHub/Pay/PayController.swift
@@ -14,7 +14,9 @@ class PayController: SuperController {
         btn.setTitle("Restore", for: .normal)
         btn.titleLabel?.font = UIFont.GoogleSans(weight: .medium, size: 12)
         btn.setTitleColor(UIColor.rgbHex("#181818"), for: .normal)
-        btn.layer.cornerRadius = 1
+        btn.layer.cornerRadius = 14
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor.rgbHex("#181818", 0.5).cgColor
         btn.layer.masksToBounds = true
         return btn
     }()
@@ -26,10 +28,15 @@ class PayController: SuperController {
         return view
     }()
     
+    lazy var iconV: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: "pre")
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
     override func initUI() {
@@ -37,5 +44,23 @@ class PayController: SuperController {
         self.bgImgV.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
+        self.navbar.bgView.addSubview(self.stroeBtn)
+        self.stroeBtn.snp.makeConstraints { make in
+            make.right.equalTo(-16)
+            make.size.equalTo(CGSize(width: 68, height: 28))
+            make.centerY.equalToSuperview()
+        }
+        self.stroeBtn.addTarget(self, action: #selector(clickStoreAction), for: .touchUpInside)
+        
+        self.view.addSubview(self.iconV)
+        self.iconV.snp.makeConstraints { make in
+            make.right.equalTo(-24)
+            make.size.equalTo(CGSize(width: 84, height: 84))
+        }
+    }
+    
+    @objc func clickStoreAction() {
+        
     }
 }
